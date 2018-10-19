9a6d7b3f8ef5d9e9c87453a55b12102ec12dc88b
Diego Wesselka <diegowesselka@outlook.com>
2018-10-19 00:05:50 -0300

    adicionando logica

--- src/app/caixa/comanda/comanda.component.ts
+++ src/app/caixa/comanda/comanda.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProducts } from './products';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-comanda',
@@ -7,9 +9,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComandaComponent implements OnInit {
 
-  constructor() { }
+  products: IProducts[];
+  total:number = 0;
+  
+  constructor() {
+    this.products = [
+      {
+        product_id: 1,
+        product_name : "Couvert",
+        product_price : 10,
+        product_subtotal : 10,
+        product_quality : 3
+      },
+      {
+        product_id: 2,
+        product_name : "Skol 600ml",
+        product_price : 12,
+        product_subtotal : 12,
+        product_quality : 1
+      },
+      {
+        product_id: 3,
+        product_name : "X Salada",
+        product_price : 10,
+        product_subtotal : 10,
+        product_quality : 1
+      },
+      {
+        product_id: 4,
+        product_name : "Original 600ml",
+        product_price : 15,
+        product_subtotal : 15,
+        product_quality : 2
+      },
+      
+    ];
+   }
 
   ngOnInit() {
+    this.totalPrice();
   }
+  
+  delproduct(pid){
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].product_id === pid)
+      {  
+        this.products.splice(i,1);
+      }           
+    }
+    this.totalPrice();
+  }
+
+
+  totalPrice(){
+    this.total = 0;
+    for(var i=0;i<this.products.length;i++){
+      const subtotal = this.products[i].product_price * this.products[i].product_quality;
+      this.total += (subtotal);
+      this.products[i].product_subtotal = subtotal;
+    }
+  }
+
+  add(pid){
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].product_id === pid)
+      {  
+        this.products[i].product_quality += 1;
+      }           
+    }
+    this.totalPrice();
+  }
+
+  del(pid){
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].product_id === pid)
+      {  
+        this.products[i].product_quality -= 1;
+      }           
+    }
+    this.totalPrice();
+  }
+
 
 }
---
+++ src/app/caixa/comanda/products.ts
@@ -0,0 +1,7 @@
+export interface IProducts {
+    product_id : number;
+    product_name : string;
+    product_price : number;
+    product_subtotal : number;
+    product_quality : number;
+}
