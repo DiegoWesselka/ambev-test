363180aaae750dd3d6df21811773341385a5ce03
Diego Wesselka <diegowesselka@outlook.com>
2018-10-19 00:29:56 -0300

    inserido mock de comanda

--- src/app/caixa/pagamento/pagamento.component.ts
+++ src/app/caixa/pagamento/pagamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pagamento',
@@ -6,12 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagamento.component.scss']
 })
 export class PagamentoComponent implements OnInit {
-
-  constructor() { }
   
   private title = 'Pagamento'
+  total:number = 0;
+  
+  @Input() recebeProdutos
+  constructor() { }
   
   ngOnInit() {
+    this.totalPrice();
+    console.log(this.recebeProdutos, 'se liga');
   }
 
+
+  totalPrice(){
+    this.total = 0;
+    for(var i=0;i<this.recebeProdutos.length;i++){
+      const subtotal = this.recebeProdutos[i].product_price * this.recebeProdutos[i].product_quality;
+      this.total += (subtotal);
+      this.recebeProdutos[i].product_subtotal = subtotal;
+    }
+  }
 }
